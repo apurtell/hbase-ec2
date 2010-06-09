40add1c92eafdd3f31cf601286b03a6fc38c39f9
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-06-08 17:45:29 -0700

    working on launching clusters in child processes

--- test/hdfs.rb
+++ test/hdfs.rb
@@ -6,7 +6,7 @@ class Cluster
   def initialize( options = {} )
     options = { 
       :num_region_servers => 5,
-      :num_zookeepers => 0
+      :num_zookeepers => 1
     }.merge(options)
     
     @name = options[:name]
@@ -14,21 +14,34 @@ class Cluster
     @num_zookeepers = options[:num_zookeepers]
 
     raise ArgumentError, "No :name provided" if options[:name].nil? || options[:name].nil?
+
+    puts "Cluster '#{@name}' ready to launch()."
+
   end
 
   def launch()
-
-
+    if fork
+      #parent.
+      puts "forked process to launch cluster: #{@name}.."
+    else
+      #child
+      exec("~/hbase-ec2/bin/hbase-ec2 launch-cluster #{@name} #{@num_region_servers} #{@num_zookeepers}")
+    end
   end
 
   def run_test(name)
   end
 
-  def terminate()
+  def terminate
+    exec("~/hbase-ec2/bin/hbase-ec2 terminate-cluster #{@name}")
+    puts "stopping cluster: #{@name}"
   end
+
+  def to_s
+    "Cluster: name=#@name; region servers: #@num_region_servers; zoo keepers: #@num_zookeepers"
+  end
+
 end
 
-puts 'hello'
-mycluster = Cluster.new(:name => "hdfs")
-puts mycluster.to_s
+
 
