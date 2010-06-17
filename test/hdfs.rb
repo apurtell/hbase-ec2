2c5521bb5dca8345c1110844000d8563dda44dab
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-06-17 11:30:42 -0700

    working on unit testing

--- test/hdfs.rb
+++ test/hdfs.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/ruby
 
 load("~/hbase-ec2/lib/hcluster.rb");
-c = AWS::EC2::Base::HCluster
-c['hdfs2'].status
-c['hdfs2'].launch
-c['hdfs2'].run_test("TestDFSIO -write -nrFiles 10 -fileSize 1000")
+hdfs = AWS::EC2::Base::HCluster.new("hdfs");
+hdfs.status
+hdfs.launch
+hdfs.run_test("TestDFSIO -write -nrFiles 10 -fileSize 1000")
+hdfs.terminate
 
 
 
