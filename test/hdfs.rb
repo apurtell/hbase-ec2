8b23a2bd20d45fc93c1e0b71acac41f39bee2d2a
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-06-17 14:20:37 -0700

    add launchTime testing

--- test/hdfs.rb
+++ test/hdfs.rb
@@ -5,22 +5,54 @@ require 'test/unit'
 load("~/hbase-ec2/lib/hcluster.rb");
 
 class TestHCluster < Test::Unit::TestCase
+  @@security_group = "hdfs"
+  @@cluster = nil
+
   def test_init
 
-    security_group = "hdfs"
-
-    hdfs = AWS::EC2::Base::HCluster.new(security_group)
+    @@cluster = AWS::EC2::Base::HCluster.new(@@security_group)
     # show status
-    status = hdfs.status
+    status = @@cluster.status
 
     status.keys.each { |key|
       puts "#{key} => #{status[key]}"
     }
 
-    assert_equal(security_group,status['name'])
-    assert(("terminated" == status['state']) || ("Initialized" == status['state']))
-    assert((nil == status['launchTime']) || ("2010-06-17T18:45:13.000Z" == status['launchTime']))
+    assert_equal(@@security_group,status['name'])
+
+    if ("running" == status['state'])
+      puts "terminating running cluster.."
+      @@cluster.terminate
+      sleep 10
+      puts "continuing."
+
+      status = @@cluster.status
+      
+      status.keys.each { |key|
+        puts "#{key} => #{status[key]}"
+      }
+     
+    end
+
+    assert(("terminated" == status['state']) || ("Initialized" == status['state']) || ("shutting-down" == status['state']))
+    launchTime = status['launchTime']
+    # second part of this disjunction is indended to only be true if launchTime is
+    # a valid EC-2 returned launching time, e.g.: "2010-06-17T21:03:56.000Z"
+    assert((nil == launchTime) || Time.parse(Time.parse(launchTime).to_s) == Time.parse(launchTime))
+
+
+
+
   end
+
+
+
+#  def test_launch
+#    @@cluster.launch
+#    assert(true)
+#  end
+  
+
 end
 
 
