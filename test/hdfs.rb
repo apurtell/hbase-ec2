24b9af264c3b6350df1aeca106c9fe0ad7c87d94
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-06-17 12:14:33 -0700

    finished first test (test_init)

--- test/hdfs.rb
+++ test/hdfs.rb
@@ -1,10 +1,20 @@
 #!/usr/bin/ruby
 
+require 'test/unit'
+
 load("~/hbase-ec2/lib/hcluster.rb");
-hdfs = AWS::EC2::Base::HCluster.new("hdfs");
-# show status
-status = hdfs.status
-status.keys.each {|key| puts "#{key} => #{status[key]}"}
+
+class TestHCluster < Test::Unit::TestCase
+  def test_init
+    hdfs = AWS::EC2::Base::HCluster.new("hdfs");
+    # show status
+    status = hdfs.status
+    assert_equal("hdfs",status['name'])
+    assert(("terminated" == status['state']) || ("Initialized" == status['state']))
+  end
+end
+
+
 #hdfs.launch
 #hdfs.run_test("TestDFSIO -write -nrFiles 10 -fileSize 1000")
 #hdfs.terminate
