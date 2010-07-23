82b5ba3199ac7d2f442078b8cebe73ad4be00256
Gary Helmling <gary_helmling@trendmicro.com>
2010-07-23 10:40:31 -0700

    Simple test example for secure hadoop/hbase recipe

---
+++ recipes/secure_hbase_test.rb
@@ -0,0 +1,24 @@
+load File.dirname(__FILE__)+'/../lib/TestDFSIO.rb'
+include Hadoop
+
+options = {
+  :label => 'hbase-us-east-1-0.21-S-append-SNAPSHOT-x86_64', 
+  :availability_zone => 'us-east-1b',
+  :num_regionservers => 3
+}
+cluster = TestDFSIO.new options
+cluster.launch
+
+cluster.ssh("cd /usr/local/hadoop-*; kinit -k -t conf/nn.keytab hadoop/#{cluster.master.privateDnsName.downcase}; bin/hadoop fs -mkdir /hbase; bin/hadoop fs -chown hbase /hbase")
+cluster.ssh("/usr/local/hbase-*/bin/hbase-daemon.sh start master")
+cluster.slaves.each {|slave|
+  @hcluster.ssh_to(slave.dnsName, "/usr/local/hbase-*/bin/hbase-daemon.sh start regionserver")}
+
+res = cluster.test
+puts 'Results:'
+puts res['pairs']
+puts 'Output:'
+puts res['stdout']
+
+cluster.terminate
+
