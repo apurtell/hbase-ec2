9dd74b1125b2a7e689843d920804404634d76977
Gary Helmling <gary_helmling@trendmicro.com>
2010-07-27 16:14:06 -0700

    specific start of tm-3 amis

---
+++ recipes/secure_hbase_tm-3.rb
@@ -0,0 +1,13 @@
+options = {
+  :label => 'hbase-0.20-tm-3-x86_64', 
+  :availability_zone => 'us-east-1b',
+  :num_regionservers => 3,
+  :owner_id => '801535628028'
+}
+cluster = @hcluster.new options
+cluster.launch
+
+cluster.ssh("cd /usr/local/hadoop-*; kinit -k -t conf/nn.keytab hadoop/#{cluster.master.privateDnsName.downcase}; bin/hadoop fs -mkdir /hbase; bin/hadoop fs -chown hbase /hbase")
+cluster.ssh("/usr/local/hbase-*/bin/hbase-daemon.sh start master")
+cluster.slaves.each {|slave|
+  @hcluster.ssh_to(slave.dnsName, "/usr/local/hbase-*/bin/hbase-daemon.sh start regionserver")}
