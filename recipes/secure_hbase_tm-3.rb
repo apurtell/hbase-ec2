a17af5d23d80822d71765d7d847cdaae628e112a
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-08-03 12:47:49 -0700

    use 'cluster', not '@hcluster' for portability.

--- recipes/secure_hbase_tm-3.rb
+++ recipes/secure_hbase_tm-3.rb
@@ -9,4 +9,5 @@ cluster.launch
 cluster.ssh("cd /usr/local/hadoop-*; kinit -k -t conf/nn.keytab hadoop/#{cluster.master.privateDnsName.downcase}; bin/hadoop fs -mkdir /hbase; bin/hadoop fs -chown hbase /hbase")
 cluster.ssh("/usr/local/hbase-*/bin/hbase-daemon.sh start master")
 cluster.slaves.each {|slave|
-  @hcluster.ssh_to(slave.dnsName, "/usr/local/hbase-*/bin/hbase-daemon.sh start regionserver")}
+  cluster.ssh_to(slave.dnsName,
+                 "/usr/local/hbase-*/bin/hbase-daemon.sh start regionserver")}
