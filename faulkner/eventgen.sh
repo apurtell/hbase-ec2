8b9cf20c44eca61577b2e4b6046d73eefa10fc25
Andrew Purtell <apurtell@apache.org>
2010-08-07 12:48:59 -0700

    size the heap correctly for eventgen

--- faulkner/eventgen.sh
+++ faulkner/eventgen.sh
@@ -1,1 +1,1 @@
-HBASE_OPTS="-Xmx2000" hbase shell ./faulkner.rb --keygen=ts --dist=zipf2 --min=64 --max=65536 --threads=20 --maxRegions=5000 --debug=true > /mnt/hbase/logs/faulkner.log 2>&1 & tail -f /mnt/hbase/logs/faulkner.log 
+HBASE_OPTS="-Xmx2000m" hbase shell ./faulkner.rb --keygen=ts --dist=zipf2 --min=64 --max=65536 --threads=20 --maxRegions=5000 --debug=true > /mnt/hbase/logs/faulkner.log 2>&1 & tail -f /mnt/hbase/logs/faulkner.log 
